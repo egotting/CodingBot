79a79993fd0ef0625cc3ddd1ec9accad3213c505
if <hnerisantos@gmail.com>
2024-01-31 17:57:44 -0300

    Basic Start CodingBot

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import bot "example.com/hello_world_bot/Bot"
+
+func main(){
+	
+	bot.BotToken = "MTIwMTU2MDMyMDMyNjUwNDQ0OA.GLixKa.hKUbioQgc48VwWFS33XYWU9HRhh7ez83E3SPko"
+	bot.Run()
+}
