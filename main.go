41fb5be1182f3c87ee5de90eeba4a31a3e724cf3
fantaasyyy <henricodss@gmail.com>
2024-02-14 15:21:00 -0300

    update

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import bot "example.com/hello_world_bot/Bot"
-
-func main(){
-	
-	bot.BotToken = "MTIwMTU2MDMyMDMyNjUwNDQ0OA.GLixKa.hKUbioQgc48VwWFS33XYWU9HRhh7ez83E3SPko"
-	bot.Run()
+func main() {
+	StartBot()
 }
