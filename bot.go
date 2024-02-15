2ddbb305019491bf04d055370a01348dff9e5db4
fantaasyyy <henricodss@gmail.com>
2024-02-15 13:55:00 -0300

    update

--- bot.go
+++ bot.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -32,17 +31,4 @@ func StartBot() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
-}
-
-func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
-	if message.Author.ID == discord.State.User.ID {
-		return
-	}
-
-	switch {
-	case strings.Contains(message.Content, "!hello"):
-		discord.ChannelMessageSend(message.ChannelID, "hello world!")
-	case strings.Contains(message.Content, "!bye"):
-		discord.ChannelMessageSend(message.ChannelID, "bye!")
-	}
 }
